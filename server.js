550ebdb69ff7e42ad39aded790234be63efbdd02
p1pawan23071 <p1lappy2333@gmail.com>
2025-06-23 08:31:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,34 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
+
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
 app.use(express.static('public'));
 
-io.on('connection', socket => {
+io.on('connection', (socket) => {
     console.log('A user connected');
 
     socket.on('join-room', ({ username, room }) => {
         socket.join(room);
-        socket.to(room).emit('message', `${username} joined the room`);
+        socket.to(room).emit('notification', `${username} joined the room`);
+    });
 
-        socket.on('chat-message', msg => {
-            io.to(room).emit('message', `${username}: ${msg}`);
+    socket.on('chat-message', ({ message, room }) => {
+        io.to(room).emit('message', { 
+            username: socket.handshake.query.username, 
+            message 
         });
+    });
 
-        socket.on('typing', () => {
-            socket.to(room).emit('typing', `${username} is typing...`);
-        });
+    socket.on('typing', ({ room }) => {
+        socket.to(room).emit('typing', socket.handshake.query.username);
+    });
 
-        socket.on('disconnect', () => {
-            io.to(room).emit('message', `${username} left the chat`);
-        });
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
     });
 });
 
